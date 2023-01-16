f90927f4ebf1d7b29f8859f8c0d5b31414e2c254
shadattonmoy <shadat.tonmoy@gmail.com>
2023-01-16 12:56:50 +0600

    completed creating multiple view controller

---
+++ ClassDemo/ClassDemo/Enemy.swift
@@ -0,0 +1,25 @@
+//
+//  Enemy.swift
+//  ClassDemo
+//
+//  Created by Shadat Tonmoy on 1/16/23.
+//
+
+import Foundation
+
+class Enemy{
+    
+    var health = 100
+    var attackStrength = 10
+    
+    
+    func move() {
+        print("Enemy is moving....")
+        
+    }
+    
+    func attack(){
+        print("Enemy is attacking...")
+        
+    }
+}
