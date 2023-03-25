eb03a9feb255db07566aad013e290a36a7a78dc2
shadattonmoy <shadat.tonmoy@gmail.com>
2023-03-25 12:08:24 +0600

    completed swift UI extracting to function

--- 00.StoryBoard_101/10.SwiftUI/SwiftUIText/SwiftUIText/SwiftUIState.swift
+++ 00.StoryBoard_101/10.SwiftUI/SwiftUIText/SwiftUIText/SwiftUIState.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct SwiftUIState: View {
     @State var countValue = 1
     @State var colorIndex = 0
-    let backgroundColors : [Color] = [Color.red, Color.teal, Color.gray, Color.purple, Color.orange, Color.green, Color.blue]
+    let backgroundColors : [Color] = [Color.red, Color.teal, Color.gray, Color.purple, Color.orange, Color.green, Color.blue, Color.yellow, Color.pink, Color.brown, Color.cyan]
     
     var body: some View {
         ZStack{
@@ -19,61 +19,90 @@ struct SwiftUIState: View {
                 .edgesIgnoringSafeArea(.all)
             
             VStack(spacing: 20){
-                Text("Count is \(countValue)")
-                    .foregroundColor(.white)
-                    .padding()
-                    .font(.title)
                 
-                Button(action: {
-                    self.countValue += 1
-                    
-                }, label: {
-                    Text("Tap to increase")
-                        .padding()
-                        .foregroundColor(backgroundColors[colorIndex])
-                        .background(
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(.white)
-                                .shadow(radius: 5)
-                         )
-                })
+                countText
                 
-                Button(action: {
-                    self.countValue -= 1
-                    if(countValue <= 0) {
-                        countValue = 0
-                        
-                    }
-                    
-                }, label: {
-                    Text("Tap to decrease")
-                        .padding()
-                        .foregroundColor(.white)
-                        .background(
-                            Capsule()
-                                .stroke(.white,lineWidth: 2)
-                        )
-                    
-                })
+                incrementButton
                 
-                Button(action: {
-                    colorIndex += 1
-                    if(colorIndex > backgroundColors.count-1){
-                        colorIndex = 0
-                    }
-                    
-                }, label: {
-                    Text("Change Color")
-                        .padding()
-                        .foregroundColor(backgroundColors[colorIndex])
-                        .background(
-                            RoundedRectangle(cornerRadius: 15)
-                                .fill(.white)
-                        )
-                })
+                decrementButton
+                
+                changeColorButton
             }
             
         }
+    }
+    
+    func updateBackgroundColor(){
+        colorIndex += 1
+        if(colorIndex > backgroundColors.count-1){
+            colorIndex = 0
+        }
+    }
+    
+    var incrementButton : some View{
+        
+        func incrementCount(){
+            self.countValue += 1
+        }
+        
+        return Button(action: {
+           incrementCount()
+            
+        }, label: {
+            Text("Tap to increase")
+                .padding()
+                .foregroundColor(backgroundColors[colorIndex])
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.white)
+                        .shadow(radius: 5)
+                 )
+        })
+    }
+    
+    var decrementButton : some View{
+        func decrementCount(){
+            self.countValue -= 1
+            if(countValue <= 0) {
+                countValue = 0
+            }
+        }
+        
+        return Button(action: {
+            decrementCount()
+            
+        }, label: {
+            Text("Tap to decrease")
+                .padding()
+                .foregroundColor(.white)
+                .background(
+                    Capsule()
+                        .stroke(.white,lineWidth: 2)
+                )
+            
+        })
+    }
+    
+    var changeColorButton : some View{
+        Button(action: {
+            updateBackgroundColor()
+            
+        }, label: {
+            Text("Change Color")
+                .padding()
+                .foregroundColor(backgroundColors[colorIndex])
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(.white)
+                )
+        })
+    }
+    
+    var countText : some View{
+        Text("Count is \(countValue)")
+            .foregroundColor(.white)
+            .padding()
+            .font(.title)
     }
 }
 
