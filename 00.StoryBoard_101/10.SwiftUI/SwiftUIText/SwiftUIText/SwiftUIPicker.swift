73fef614a694065036fed8ee7c4c4e197d63b726
Shadat Tonmoy <shadat.tonmoy@konasl.com>
2023-03-28 15:19:44 +0600

    completed picker in swift

--- 00.StoryBoard_101/10.SwiftUI/SwiftUIText/SwiftUIText/SwiftUIPicker.swift
+++ 00.StoryBoard_101/10.SwiftUI/SwiftUIText/SwiftUIText/SwiftUIPicker.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct SwiftUIPicker: View {
+    @State var selection : String = "1"
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack{
+            Text("Selected Item : \(selection)")
+            
+//            Picker(selection: $selection, label: Text("Picker"), content: {
+//                Text("1").tag("1")
+//                Text("2").tag("2")
+//                Text("3").tag("3")
+//            })
+//            .pickerStyle(.menu)
+            
+            Picker(selection: $selection, content: {
+                ForEach(18..<100, content: { index in
+                    Text("\(index)")
+                })
+                
+            }, label: {
+                Text("Picker")
+            }).pickerStyle(.wheel)
+        }
+        
+       
     }
 }
 
