0508b4c57c41599a598b9a342227bc46452662f1
Shadat Tonmoy <shadat.tonmoy@konasl.com>
2023-03-28 09:10:43 +0600

    started swift ui picker

---
+++ 00.StoryBoard_101/10.SwiftUI/SwiftUIText/SwiftUIText/SwiftUIPicker.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIPicker.swift
+//  SwiftUIText
+//
+//  Created by Tonmoy on 28/3/23.
+//
+
+import SwiftUI
+
+struct SwiftUIPicker: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIPicker_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIPicker()
+    }
+}
