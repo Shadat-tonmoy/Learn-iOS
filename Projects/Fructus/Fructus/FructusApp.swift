1db3ac2f0286f1d1918aaddeca4e882084839e1d
Shadat Tonmoy <shadat.tonmoy@konasl.com>
2023-04-03 08:53:29 +0600

    added projets

---
+++ Projects/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Tonmoy on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
