1ae4f7b1c9927fbe1b73ff47c3845992efee58c8
shadattonmoy <shadat.tonmoy@gmail.com>
2023-04-20 15:38:00 +0600

    completed implementation of swithching grid type in home screen animal list

--- Projects/Africa/Africa/App/ContentView.swift
+++ Projects/Africa/Africa/App/ContentView.swift
@@ -10,22 +10,47 @@ import SwiftUI
 struct ContentView: View {
     
     let animal : [Animal] = Bundle.main.decode("animals.json")
+    @State var animalGridItems : [GridItem] = Array(repeating: GridItem(.flexible()), count: 2)
     @State var isGridActivated = false
+    @State var gridCellCount = 2
     
     var body: some View {
         NavigationView {
-            List() {
-                CoverImageView()
-                    .frame(width: 400, height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            
+            Group{
+                if !isGridActivated {
+                    List() {
+                        CoverImageView()
+                            .frame(width: 400, height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                                        
+                        ForEach(animal, id: \.self) { animal in
+                            NavigationLink(destination : AnimalDetailView(animal: animal)){
+                                AnimalListItemView(animal: animal)
+                            }
+                        }
+                        
+                    }
+                } else {
+                    ScrollView(.vertical, showsIndicators: false){
+                        LazyVGrid(columns: animalGridItems, content: {
+                            ForEach(animal) { animal in
+                                NavigationLink(destination : AnimalDetailView(animal: animal)){
+                                    AnimalGridItemView(animal: animal)
+                                }
                                 
-                ForEach(animal, id: \.self) { animal in
-                    NavigationLink(destination : AnimalDetailView(animal: animal)){
-                        AnimalListItemView(animal: animal)
+                            }
+                        }).onAppear{
+                            withAnimation(.easeIn(duration: 2)){
+                                
+                            }
+                        }
+                        
                     }
                 }
                 
-            }.navigationTitle("Africa")
+            }
+            .navigationTitle("Africa")
                 .listStyle(.inset)
                 .navigationBarItems(trailing: HStack{
                     Button(action: {
@@ -38,14 +63,37 @@ struct ContentView: View {
                     })
                     
                     Button(action: {
-                        isGridActivated = true
+                        if(!isGridActivated){
+                            isGridActivated = true
+                        } else {
+                            switchGridType()
+                        }
                         
                     }, label: {
-                        Image(systemName: "square.grid.2x2")
+                        Image(systemName: getGridIcon())
                             .font(.title2)
                             .foregroundColor(isGridActivated ? .accentColor : .white)
                     })
                 })
+        }
+    }
+    
+    func switchGridType(){
+        gridCellCount += 1
+        gridCellCount %= 4
+        if(gridCellCount == 0) {
+            gridCellCount = 1
+        }
+        animalGridItems = Array(repeating: .init(.flexible()), count: gridCellCount)
+    }
+    
+    func getGridIcon() -> String{
+        if(gridCellCount == 1){
+            return "rectangle.grid.1x2"
+        } else if(gridCellCount == 3){
+            return "square.grid.3x2"
+        } else {
+            return "square.grid.2x2"
         }
     }
 }
