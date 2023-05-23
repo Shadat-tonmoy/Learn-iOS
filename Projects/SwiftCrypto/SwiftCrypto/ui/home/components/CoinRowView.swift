76c7204cf69aed09367d75c37cbcc97f5ba620da
shadattonmoy <shadat.tonmoy@gmail.com>
2023-05-23 21:52:29 +0600

    completed lesson 3 (creating a single coin row)

---
+++ Projects/SwiftCrypto/SwiftCrypto/ui/home/components/CoinRowView.swift
@@ -0,0 +1,85 @@
+//
+//  CoinRowView.swift
+//  SwiftCrypto
+//
+//  Created by Shadat Tonmoy on 5/23/23.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    
+    let coin : CoinModel
+    let showHoldings : Bool
+    
+    var body: some View {
+        HStack(spacing : 0){
+            
+            leftColumn
+            
+            Spacer()
+            
+            if showHoldings {
+                centerColumn
+            }
+            
+            rightColumn
+            
+            
+            
+            
+            
+        }
+    }
+}
+
+
+extension CoinRowView {
+    var leftColumn : some View {
+        HStack{
+            Text("\(coin.rank)")
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+                .frame(width:30)
+            
+            Circle()
+                .frame(width: 30, height: 30)
+            
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(.theme.accent)
+                .padding(.leading, 6)
+        }
+    }
+    
+    var rightColumn : some View{
+        VStack(alignment : .trailing){
+            Text("\(coin.currentPrice.asCurrencyWithDecimal())")
+                .fontWeight(.bold)
+                .foregroundColor(.theme.accent)
+            
+            
+            Text(coin.priceChangePercentage24HInCurrency?.asPercentString() ?? "0.0")
+                .foregroundColor(
+                    coin.negativePriceChange ? .theme.red : .theme.green
+                )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3, alignment: .trailing)
+    }
+    
+    var centerColumn : some View{
+        VStack(alignment : .trailing){
+            Text(coin.currentHoldingsValue.asCurrencyWithDecimal())
+                .bold()
+            
+            Text(coin.currentHoldings?.asNumberString() ?? "")
+        }
+    }
+    
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: developerPreview.coin, showHoldings: true)
+    }
+}
