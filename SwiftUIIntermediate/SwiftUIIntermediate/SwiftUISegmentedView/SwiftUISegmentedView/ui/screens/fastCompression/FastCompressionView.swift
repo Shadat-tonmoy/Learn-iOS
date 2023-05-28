b0ec9f027900ab899e377e00d1152075ad4bf09a
shadattonmoy <shadat.tonmoy@gmail.com>
2023-05-28 19:46:06 +0600

    separate out tab view ui logic into standalone file

--- SwiftUIIntermediate/SwiftUIIntermediate/SwiftUISegmentedView/SwiftUISegmentedView/ui/screens/fastCompression/FastCompressionView.swift
+++ SwiftUIIntermediate/SwiftUIIntermediate/SwiftUISegmentedView/SwiftUISegmentedView/ui/screens/fastCompression/FastCompressionView.swift
@@ -13,32 +13,14 @@ struct FastCompressionView: View {
     @StateObject private var viewModel : FastCompressionViewModel = FastCompressionViewModel()
     
     
-    
     var body: some View {
         
         VStack(spacing: 0) {
-            NavbarWithBackButton(title: "Fast Compression")
-            HStack(){
-                ForEach(0..<viewModel.tabs.count, id: \.self, content: { index in
-                    Text(viewModel.tabs[index])
-                        .frame(maxWidth: .infinity)
-                        .foregroundColor(getForegroundColor(index))
-                        .padding(.vertical, 12)
-                        .overlay(getBackgroundColor(index), alignment: .bottom)
-                        .font(.subheadline)
-                        .fontWeight(.medium)
-                        .onTapGesture {
-                            withAnimation(.easeInOut(duration: 0.1)){
-                                viewModel.selectedTab = index
-                            }
-                            
-                            
-                        }
-                    
-                })
-            }
-            .background(Color.toolbarBG)
             
+            NavbarWithBackButton(title: "Fast Compression")
+            
+            FastCompressionTabView(viewModel: viewModel)
+                      
             CompressionPropertyView(optionCallback: propertyOptionCallback)
             
             getSelectedTabView()
@@ -69,23 +51,6 @@ struct FastCompressionView: View {
             AdjustVolumeScreen(showSheet: $viewModel.showAdjustVolumeOption)
         })
         
-    }
-    
-    private func isSelected (position : Int) -> Bool {
-        return position == viewModel.selectedTab
-    }
-    
-    private func getForegroundColor(_ position : Int) -> Color {
-        return isSelected(position: position) ? .white : .white
-    }
-    
-    private func getBackgroundColor(_ position : Int) -> AnyView {
-        return isSelected(position: position) ? AnyView(
-            RoundedRectangle(cornerRadius: 12)
-                .fill(.white)
-                .frame(height: 4)
-            
-        ) : AnyView(EmptyView())
     }
     
     private func getSelectedTabView() -> AnyView {
--- SwiftUIIntermediate/SwiftUIIntermediate/SwiftUISegmentedView/SwiftUISegmentedView/ui/screens/fastCompression/FastCompressionViewModel.swift
+++ SwiftUIIntermediate/SwiftUIIntermediate/SwiftUISegmentedView/SwiftUISegmentedView/ui/screens/fastCompression/FastCompressionViewModel.swift
@@ -58,5 +58,4 @@ class FastCompressionViewModel : ObservableObject {
         showSpeedOptions = videoPropertyOption == Values.SPEED_OPTIONS
     }
     
-    
 }
