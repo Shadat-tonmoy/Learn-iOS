e57ee8c8a8bde85e8df38464c875d458fc494c65
shadattonmoy <shadat.tonmoy@gmail.com>
2025-04-05 23:06:16 +0600

    - completed simple login screen with storyboard

---
+++ CodingWithCrisPractice/SimpleLoginPage/SimpleLoginPage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleLoginPage
+//
+//  Created by Shadat Tonmoy on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodingWithCrisPractice/SimpleLoginPage/SimpleLoginPageTests/SimpleLoginPageTests.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleLoginPageTests.swift
+//  SimpleLoginPageTests
+//
+//  Created by Shadat Tonmoy on 4/5/25.
+//
+
+import Testing
+@testable import SimpleLoginPage
+
+struct SimpleLoginPageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
