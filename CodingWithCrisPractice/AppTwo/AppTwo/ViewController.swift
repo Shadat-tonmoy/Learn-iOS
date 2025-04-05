c9e6b894e8fb3f8ba4f781a117adc7eabd132ab9
shadattonmoy <shadat.tonmoy@gmail.com>
2025-04-05 23:30:11 +0600

    - completed apptwo

---
+++ CodingWithCrisPractice/AppTwo/AppTwo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by Shadat Tonmoy on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ CodingWithCrisPractice/AppTwo/AppTwoTests/AppTwoTests.swift
@@ -0,0 +1,17 @@
+//
+//  AppTwoTests.swift
+//  AppTwoTests
+//
+//  Created by Shadat Tonmoy on 4/5/25.
+//
+
+import Testing
+@testable import AppTwo
+
+struct AppTwoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
